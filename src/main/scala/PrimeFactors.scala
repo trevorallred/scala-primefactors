32f07d51dbca98be1a58f813655975b7818b8383
Trevor Allred <trevorallred@gmail.com>
2013-11-02 15:35:14 -0700

    refactor methods but test 9 is still failing

--- src/main/scala/PrimeFactors.scala
+++ src/main/scala/PrimeFactors.scala
@@ -1,13 +1,16 @@
 object PrimeFactors {
   def of(number: Int): List[Int] = {
-    if (number == 1) {
-      List()
-    } else {
-      if (number % 2 == 0) {
-        List(2) ::: of(number / 2)
+    def breakdown(number: Int, divisor: Int): List[Int] = {
+      if (number == 1) {
+        List()
       } else {
-        List(number)
+        if (number % divisor == 0) {
+          List(divisor) ::: of(number / divisor)
+        } else {
+          List(number)
+        }
       }
     }
+    breakdown(number, 2)
   }
 }
