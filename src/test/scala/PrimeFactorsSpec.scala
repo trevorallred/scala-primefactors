bb2eff7949feb7740ea6b40ad71f759d0e0bd504
Trevor Allred <trevorallred@gmail.com>
2013-11-02 15:22:56 -0700

    second failing test

--- src/test/scala/PrimeFactorsSpec.scala
+++ src/test/scala/PrimeFactorsSpec.scala
@@ -5,4 +5,7 @@ class PrimeFactorsSpec extends PropSpec with ShouldMatchers {
   property("Prime Factors for 1") {
     PrimeFactors of 1 should equal(List())
   }
+  property("Prime Factors for 2") {
+    PrimeFactors of 2 should equal(List(2))
+  }
 }
